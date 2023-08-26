85f396b45790c66b6ba5c9e8e39599d6ea3b2a0e
Ming Jing <jingming.cn@gmail.com>
2023-08-26 14:40:27 +0800

    Create match_label.py

---
+++ match_label.py
@@ -0,0 +1,10 @@
+import cv
+
+filename_label = {}
+
+with open('filename-label.csv',newline='') as csvfile:
+  csvreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+    for row in csvreader:
+      
+
+  filename_label
